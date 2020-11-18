6e3078a90dc24c042c2f792426a56a3ac02fbda3
John Choi <johnchoi1003@icloud.com>
2020-11-18 03:20:39 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GTA-Public-Solo-Initializer
 Batch scripts that will trigger GTA 5 to put current player in a separate public server with no one in it.
+
+## Instructions
+
+#### First time setup
+
+- Download and unzip source code.
+- Double click on `installation.bat` to install required dependencies.
+- Save `execute_serv.bat` to somewhere you can go back easily.
+- Feel free to delete `installation.bat`
+
+#### Running the script
+
+- Make sure you are in GTA Online with random players.
+- Minimize GTA and double click on `execute_serv.bat`
+- Follow on-screen instructions.
+
---
+++ installation.bat
@@ -0,0 +1,50 @@
+@echo off
+ > %temp%\~tmp.vbs echo sUrl = "https://download.sysinternals.com/files/PSTools.zip"
+>> %temp%\~tmp.vbs echo sFolder = "%USERDATA%\AppData\johnchoi\PSTools\"
+>> %temp%\~tmp.vbs (findstr "'--VBS" "%0" | findstr /v "findstr")
+cscript //nologo %temp%\~tmp.vbs
+del /q %temp%\~tmp.vbs
+goto :eof
+
+'--- figure out temp file & folder
+With CreateObject("WScript.Shell")  '--VBS
+    sTempFile = .Environment("Process").Item("TEMP") & "\file.zip"  '--VBS 
+    sTempFolder = .Environment("Process").Item("TEMP") & "\file.zip.extracted"  '--VBS
+End With    '--VBS
+
+'--- download
+With CreateObject("MSXML2.XMLHTTP") '--VBS
+    .Open "GET", sUrl, false    '--VBS
+    .Send() '--VBS
+    If .Status = 200 Then   '--VBS
+        ResponseBody = .ResponseBody    '--VBS
+        With Createobject("Scripting.FileSystemObject") '--VBS
+            If .FileExists(sTempFile) Then  '--VBS
+                .DeleteFile sTempFile   '--VBS
+            End If  '--VBS
+        End With    '--VBS
+        With CreateObject("ADODB.Stream")   '--VBS
+            .Open   '--VBS
+            .Type = 1 ' adTypeBinary    '--VBS
+            .Write ResponseBody '--VBS
+            .Position = 0   '--VBS
+            .SaveToFile sTempFile   '--VBS
+        End With    '--VBS
+    End If  '--VBS
+End With    '--VBS
+
+'--- extract
+With CreateObject("Scripting.FileSystemObject") '--VBS
+    On Error Resume Next    '--VBS
+    .CreateFolder sFolder   '--VBS
+    .DeleteFolder sTempFolder, True '--VBS
+    .CreateFolder sTempFolder   '--VBS
+    On Error GoTo 0 '--VBS
+    With CreateObject("Shell.Application")  '--VBS
+        .NameSpace(sTempFolder).CopyHere .NameSpace(sTempFile).Items    '--VBS
+    End With    '--VBS
+    .CopyFolder sTempFolder, sFolder, True  '--VBS
+    .DeleteFolder sTempFile, True   '--VBS
+    .DeleteFile sTempFile, True '--VBS
+End With    '--VBS
+
